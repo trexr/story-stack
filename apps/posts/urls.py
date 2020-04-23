b668e9cf1d7dc1d57c18396d14bcc63330015e32
trexr <6519611+trexr@users.noreply.github.com>
2020-04-23 15:53:58 -0700

    Updates views  to display Post slugs

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
-
+from django.urls import path, re_path
 from apps.posts import views
 
 urlpatterns = [
     path('', views.view_all_posts),
-
+    re_path(r'^(?P<slug>[\w-]+)/$', views.post_detail)
 ]
