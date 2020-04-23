53ea985ba153fd11e9dd26cbd15f4803da45ea96
trexr <6519611+trexr@users.noreply.github.com>
2020-04-22 18:23:06 -0700

    Updates to pull from app static file, adds mock illustrations

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -11,6 +11,43 @@ def view_all_posts(request):
             "title": "Rain Clouds",
             "story": "Let the stormy clouds chase everyone from the place Come on with the rain, I've a smile on my face I walk down the lane with a happy refrain",
 
+        },
+            {
+            "img": "drawing2-1024x1024.jpg",
+            "title": "Rain Clouds",
+            "story": "Let the stormy clouds chase everyone from the place Come on with the rain, I've a smile on my face I walk down the lane with a happy refrain",
+
+        },
+            {
+            "img": "drawing3-1024x1024.jpg",
+            "title": "Rain Clouds",
+            "story": "Let the stormy clouds chase everyone from the place Come on with the rain, I've a smile on my face I walk down the lane with a happy refrain",
+
+        },
+            {
+            "img": "drawing4-1024x1024.jpg",
+            "title": "Rain Clouds",
+            "story": "Let the stormy clouds chase everyone from the place Come on with the rain, I've a smile on my face I walk down the lane with a happy refrain",
+
+        },
+            {
+            "img": "drawing5-1024x1024.jpg",
+            "title": "Rain Clouds",
+            "story": "Let the stormy clouds chase everyone from the place Come on with the rain, I've a smile on my face I walk down the lane with a happy refrain",
+
+        },
+            {
+            "img": "drawing6-1024x1024.jpg",
+            "title": "Rain Clouds",
+            "story": "Let the stormy clouds chase everyone from the place Come on with the rain, I've a smile on my face I walk down the lane with a happy refrain",
+
+        },
+
+            {
+            "img": "drawing7-1024x1024.jpg",
+            "title": "There be demons",
+            "story": "Let the stormy clouds chase everyone from the place Come on with the rain, I've a smile on my face I walk down the lane with a happy refrain",
+
         }]
     }
 
