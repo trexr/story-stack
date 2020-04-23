f3640aef36c225c831681ea4fa925a1ee7d5ef02
trexr <6519611+trexr@users.noreply.github.com>
2020-04-23 15:52:31 -0700

    Adds new DB objects

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=142)
+    slug = models.TextField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    #add in thumbnail and author
+    # add in
+
+    def __str__(self):
+        return self.title
