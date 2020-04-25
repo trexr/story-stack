376585816cb5dbbf61e7b291ed5782e67dff08c8
trexr <6519611+trexr@users.noreply.github.com>
2020-04-24 19:35:40 -0700

    Adds edit feature to detail pages

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -6,5 +6,6 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.view_all_posts, name="list"),
     re_path(r'^create/$', views.post_create, name="create"),
-    re_path(r'^(?P<slug>[\w-]+)/$', views.post_detail)
+    re_path(r'^edit-post/(?P<slug>[\w-]+)', views.post_edit, name="edit_post"),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.post_detail),
 ]
