492af5c02413780efc4e94e94ee641ee7cbad816
trexr <6519611+trexr@users.noreply.github.com>
2020-04-26 13:16:35 -0700

    Updates posts  use ID, Adds Feed LP after login

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -8,20 +8,40 @@ from django.contrib.auth.models import User
 
 
 @login_required(login_url='/account/login/')
-def view_all_posts(request):
+def view_all_users_posts(request):
+    # author = request.user
+    authorid = request.user.id
+
+    # posts = Post.objects.filter(deleted=False).order_by('date')
+    # posts_by_user = posts.filter(author=author)
+
+    context = {
+        # 'posts_by_user': posts_by_user,
+        'authorid': authorid
+    }
+    # print(author.id)
+
+    return render(request, 'posts/view_all_users_posts.html', context)
+
+
+@login_required(login_url='/account/login/')
+def view_all_posts(request, id):
     author = request.user
+    authorid = request.user.id
+
     posts = Post.objects.filter(deleted=False).order_by('date')
     posts_by_user = posts.filter(author=author)
 
     context = {
-        'posts_by_user': posts_by_user
+        'posts_by_user': posts_by_user,
+        'authorid': authorid
     }
-    print(context['posts_by_user'])
+    print(author.id)
 
     return render(request, 'posts/view_all_posts.html', context)
 
 
-def post_detail(request, slug):
+def post_detail(request, slug, id):
     # return HttpResponse(slug)
     post = Post.objects.get(slug=slug)
     if post.deleted == True:
@@ -35,10 +55,12 @@ def post_detail(request, slug):
 
 
 @login_required(login_url='/account/login/')
-def post_edit(request, slug):
+def post_edit(request, slug, id):
     # return HttpResponse(slug)
     post = Post.objects.get(slug=slug)
+
     form = forms.CreatePost(instance=post)
+
     if post.deleted == True:
         return redirect('/403/')
 
@@ -50,7 +72,7 @@ def post_edit(request, slug):
             if 'Delete' in request.POST:
                 instance.deleted = True
             instance.save()
-            return redirect('/posts/')
+            return redirect('posts:user_list', id=id)
 
     context = {
         'form':  form,
@@ -61,20 +83,22 @@ def post_edit(request, slug):
 
 
 @login_required(login_url='/account/login/')
-def post_create(request):
+def post_create(request, id):
+    userid = request.user
     if request.method == 'POST':
         form = forms.CreatePost(request.POST, request.FILES)
         if form.is_valid():
             instance = form.save(commit=False)
-            instance.author = request.user
+            instance.author = userid
             instance.save()
-            return redirect('posts:list')
+            return redirect('posts:user_list', id=userid)
 
     else:
         form = forms.CreatePost()
 
     context = {
-        'form': form
+        'form': form,
+        'userid': userid
     }
 
     return render(request, 'posts/post_create.html', context)
