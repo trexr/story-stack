26bd4a307b3522f1e5855c94b6e9124174bfb216
trexr <6519611+trexr@users.noreply.github.com>
2020-05-01 20:31:17 -0700

    updates keys

--- storystack/config/production.py
+++ storystack/config/production.py
@@ -16,3 +16,14 @@ ANYMAIL = {
     'MAILGUN_API_KEY': os.environ.get('MAILGUN_API_KEY'),
     'MAILGUN_SENDER_DOMAIN': os.environ.get('MAILGUN_DOMAIN')
 }
+
+MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY')
+MAILGUN_SENDER_DOMAIN = os.environ.get('MAILGUN_DOMAIN')
+
+
+EMAIL_HOST = MAILGUN_SENDER_DOMAIN
+EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'testsite_app'
+# EMAIL_HOST_PASSWORD = 'mys3cr3tp4ssw0rd'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'user@storystack.com'
